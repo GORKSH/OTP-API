97a5e7be2654166cb7cf47d04f2612967072fabe
GORKSH <shubhamkumar13oct96@gmail.com>
2017-07-17 20:23:23 +0530

    google login

--- index.php
+++ index.php
@@ -1,35 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<!-- Meta -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	<title>One Time Password verification API</title>
-	<!-- Bootstrap Core CSS -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/otp.css">
+<?php
+//Include GP config file && User class
+include_once 'gpConfig.php';
+include_once 'User.php';
 
+if(isset($_GET['code'])){
+	$gClient->authenticate($_GET['code']);
+	$_SESSION['token'] = $gClient->getAccessToken();
+	header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
+}
+
+if (isset($_SESSION['token'])) {
+	$gClient->setAccessToken($_SESSION['token']);
+}
+
+if ($gClient->getAccessToken()) {
+	//Get user profile data from google
+	$gpUserProfile = $google_oauthV2->userinfo->get();
+	
+	//Initialize User class
+	$user = new User();
+	
+	//Insert or update user data to the database
+    $gpUserData = array(
+        'oauth_provider'=> 'google',
+        'oauth_uid'     => $gpUserProfile['id'],
+        'first_name'    => $gpUserProfile['given_name'],
+        'last_name'     => $gpUserProfile['family_name'],
+        'email'         => $gpUserProfile['email'],
+        'gender'        => $gpUserProfile['gender'],
+        'locale'        => $gpUserProfile['locale'],
+        'picture'       => $gpUserProfile['picture'],
+        'link'          => $gpUserProfile['link']
+    );
+    $userData = $user->checkUser($gpUserData);
+	
+	//Storing user data into session
+	$_SESSION['userData'] = $userData;
+	
+	//Render facebook profile data
+    if(!empty($userData)){
+        $output = '<h1>Google+ Profile Details </h1>';
+        $output .= '<img src="'.$userData['picture'].'" width="300" height="220">';
+        $output .= '<br/>Google ID : ' . $userData['oauth_uid'];
+        $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
+        $output .= '<br/>Email : ' . $userData['email'];
+        $output .= '<br/>Gender : ' . $userData['gender'];
+        $output .= '<br/>Locale : ' . $userData['locale'];
+        $output .= '<br/>Logged in with : Google';
+        $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Google+ Page</a>';
+        $output .= '<br/>Logout from <a href="logout.php">Google</a>'; 
+    }else{
+        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+    }
+} else {
+	$authUrl = $gClient->createAuthUrl();
+	$output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/glogin.png" alt=""/></a>';
+}
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login with Google using PHP by CodexWorld</title>
+<style type="text/css">
+h1{font-family:Arial, Helvetica, sans-serif;color:#999999;}
+</style>
 </head>
 <body>
-<h3>One Time Password verification API</h3>
-	<div id="container_otp_form">
-		<div class="form-group">
-			<label  >Your Mobile Number</label>
-			<input class="form-control unicase-form-control text-input" id="mobile_no_input_field" placeholder="Mobile Number">
-		</div>
-		<div class="form-group">
-			<label  >Enter OTP *</label>
-			<input class="form-control unicase-form-control text-input" id="otp_validate_field" placeholder="Enter OTP">
-		</div>
-		<div class="outer-xs">
-			<a href="#" class="forgot-password pull-right" id="resend_otp_btn" >Resend OTP</a>
-		</div>
-		<button  class="btn-upper btn btn-primary checkout-page-button" id="otp_validate_button">Submit</button>
-		<div>
-			<center><span id="otp_send_message"></span></center>
-		</div>
-	</div>
-	<script src="assets/js/jquery-1.11.1.min.js"></script>
-	<script src="assets/js/otp.js"></script>
+<div><?php echo $output; ?></div>
 </body>
 </html>
